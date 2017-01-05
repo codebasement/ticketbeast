180fc1e0d7361aaf658ab54420354f25d36082b0
Guy Adam <guy.adam@gmail.com>
2017-01-06 01:36:08 +1100

    Added concert listing feature test

---
+++ app/Concert.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class concert extends Model
+{
+    protected $guarded = [];
+
+    protected $dates = ['date'];
+}
--- tests/features/ViewConcertListingTest.php
+++ tests/features/ViewConcertListingTest.php
@@ -1,10 +1,49 @@
 <?php
 
+use App\Concert;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ViewConcertListingTest extends TestCase
 {
+    use DatabaseMigrations;
 
+    /** @test */
+    function user_can_view_a_concert_listing()
+    {
+        // Arrange
+        // Create a concert
+        $concert = Concert::create([
+            'title' => 'The Red Chord',
+            'subtitle' => 'with Animosity and Lethargy',
+            'date' => Carbon::parse('December 13, 2016 8:00pm'),
+            'ticket_price' => 3250,
+            'venue' => 'The Mosh Pit',
+            'venue_address' => '123 Example Lane',
+            'city' => 'Laraville',
+            'state' => 'ON',
+            'zip' => '17916',
+            'additional_information' => 'For tickets, call (555) 555-5555.',
+        ]);
+
+        // Act
+        // View the concert listing
+        $this->visit('/concerts/'.$concert->id);
+
+        // Assert outcome obtained
+        // Verify we can see the concert detail
+        $this->see('The Red Chord');
+        $this->see('with Animosity and Lethargy');
+        $this->see('December 13, 2016');
+        $this->see('8:00pm');
+        $this->see('$32.50');
+        $this->see('The Mosh Pit');
+        $this->see('123 Example Lane');
+        $this->see('Laraville');
+        $this->see('ON');
+        $this->see('17916');
+        $this->see('For tickets, call (555) 555-5555.');
+    }
 }
