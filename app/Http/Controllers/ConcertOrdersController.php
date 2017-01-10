0df2f2664a0dbfc4e57b85fad003f716726ad736
Guy Adam <guy.adam@gmail.com>
2017-01-11 00:03:46 +1100

    Cancelling Failed Orders

--- app/Http/Controllers/ConcertOrdersController.php
+++ app/Http/Controllers/ConcertOrdersController.php
@@ -41,6 +41,7 @@ class ConcertOrdersController extends Controller
             return response()->json([], 201);
 
         } catch (PaymentFailedException $e) {
+            $order->cancel();
             return response()->json([], 422);
         } catch (NotEnoughTicketsException $e) {
             return response()->json([], 422);
