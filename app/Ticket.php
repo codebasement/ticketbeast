009f6f1d4d006399a0099ad8d9d38c8dd52a971d
Guy Adam <guy.adam@gmail.com>
2017-01-11 08:02:46 +1100

    Refactoring and Redundant Test Coverage

--- app/Ticket.php
+++ app/Ticket.php
@@ -7,9 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Ticket extends Model
 {
 	protected $guarded = [];
-	
+
     public function scopeAvailable($query)
     {
     	return $query->whereNull('order_id');
+    }
+
+    public function release()
+    {
+    	$this->update(['order_id' => null]);
     }
 }
