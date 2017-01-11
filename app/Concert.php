46f17a7b5d8545a67b69a8e494321e7f7c3debd4
Guy Adam <guy.adam@gmail.com>
2017-01-12 08:59:21 +1100

    Preparing for Extraction

--- app/Concert.php
+++ app/Concert.php
@@ -33,7 +33,7 @@ class Concert extends Model
 
     public function orders()
     {
-        return $this->hasMany(Order::class);
+        return $this->belongsToMany(Order::class, 'tickets');
     }
 
     public function hasOrderFor($customerEmail)
@@ -70,9 +70,9 @@ class Concert extends Model
 
     public function createOrder($email, $tickets)
     {
-        $order = $this->orders()->create([
+        $order = Order::create([
             'email' => $email,
-            'amount' => $tickets->count() * $this->ticket_price,
+            'amount' => $tickets->sum('price'),
         ]);
 
         foreach ($tickets as $ticket) {
