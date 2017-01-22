3281cff2fccfe4c07959fb738fb532c43fc080af
Guy Adam <guy.adam@gmail.com>
2017-01-22 16:40:44 +1100

    Dealing with Lingering State

--- tests/unit/Billing/StripePaymentGatewayTest.php
+++ tests/unit/Billing/StripePaymentGatewayTest.php
@@ -7,13 +7,35 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class StripePaymentGatewayTest extends TestCase
 {
-    /** @test */
-    function charges_with_a_valid_payment_token_are_successful()
-    {
-        // Create a new stripe payment gateway instance
-    	$paymentGateway = new StripePaymentGateway(config('services.stripe.secret'));
 
-		$token = \Stripe\Token::create([
+	protected function setUp()
+	{
+		parent::setUp();
+
+		$this->lastCharge = $this->lastCharge();
+	}
+
+	private function lastCharge()
+	{
+		return \Stripe\Charge::all(
+        	['limit' => 1],
+        	['api_key' => config('services.stripe.secret')]
+        )['data'][0];
+	}
+
+	private function newCharges()
+	{
+		return \Stripe\Charge::all(
+        	[
+        		'ending_before' => $this->lastCharge ? $this->lastCharge->id : null,
+        	],
+        	['api_key' => config('services.stripe.secret')]
+        )['data'];
+	}
+
+	private function validToken()
+	{
+		return \Stripe\Token::create([
 			"card" => [
 				"number" => "4242424242424242",
 				"exp_month" => 1,
@@ -21,16 +43,19 @@ class StripePaymentGatewayTest extends TestCase
 				"cvc" => "123"
 			]
 		], ['api_key' => config('services.stripe.secret')])->id;
+	}
+
+    /** @test */
+    function charges_with_a_valid_payment_token_are_successful()
+    {
+        // Create a new stripe payment gateway instance
+    	$paymentGateway = new StripePaymentGateway(config('services.stripe.secret'));
 
         // Create a new charge for some amount using a valid token
-    	$paymentGateway->charge(2500, $token);
+    	$paymentGateway->charge(2500, $this->validToken());
 
-        // Verify the the charge was completed successfully
-        $lastCharge = \Stripe\Charge::all(
-        	['limit' => 1],
-        	['api_key' => config('services.stripe.secret')]
-        )['data'][0];
-
-        $this->assertEquals(2500, $lastCharge->amount);
+    	// Verify that the charge was completed successfully
+        $this->assertCount(1, $this->newCharges());
+        $this->assertEquals(2500, $this->lastCharge()->amount);
     }
 }
