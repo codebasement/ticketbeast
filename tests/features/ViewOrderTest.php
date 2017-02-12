8318ef61796bc7d0962e9a9398c87c38f8a4e9cc
Guy Adam <guy.adam@gmail.com>
2017-02-13 07:46:07 +1100

    Making Static Data Real

--- tests/features/ViewOrderTest.php
+++ tests/features/ViewOrderTest.php
@@ -20,11 +20,20 @@ class ViewOrderTest extends TestCase
         // create an order
         $order = factory(Order::class)->create([
         	'confirmation_number' => 'ORDERCONFIRMATION1234',
+        	'card_last_four' => '1881',
+        	'amount' => 8500,
         ]);
-        // Create a ticket
-        $ticket = factory(Ticket::class)->create([
+        // Create tickets
+        $ticketA = factory(Ticket::class)->create([
         	'concert_id' => $concert->id,
         	'order_id' => $order->id,
+        	'code' => 'TICKETCODE123',
+        ]);
+
+        $ticketB = factory(Ticket::class)->create([
+        	'concert_id' => $concert->id,
+        	'order_id' => $order->id,
+        	'code' => 'TICKETCODE456',
         ]);
 
         // Visit order confirmation page
@@ -36,5 +45,11 @@ class ViewOrderTest extends TestCase
         $response->assertViewHas('order', function ($viewOrder) use ($order) {
         	return $order->id === $viewOrder->id;
         });
+
+        $response->assertSee('ORDERCONFIRMATION1234');
+        $response->assertSee('$85.00');
+        $response->assertSee('**** **** **** 1881');
+        $response->assertSee('TICKETCODE123');
+        $response->assertSee('TICKETCODE456');
     }
 }
