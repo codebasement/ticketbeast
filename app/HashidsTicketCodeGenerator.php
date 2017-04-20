7268e19290667895df63f00b2f3595570303f23d
Guy Adam <guy.adam@gmail.com>
2017-04-20 22:07:33 +1000

    Integrating Hashids

---
+++ app/HashidsTicketCodeGenerator.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+class HashidsTicketCodeGenerator implements TicketCodeGenerator
+{
+	private $hashids;
+
+	public function __construct($salt)
+	{
+		$this->hashids = new \Hashids\Hashids($salt, 6, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+	}
+
+	
+	public function generateFor($ticket)
+	{
+		return $this->hashids->encode($ticket->id);
+	}
+}
