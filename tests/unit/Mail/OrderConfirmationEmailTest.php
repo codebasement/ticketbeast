329938a66ec4890d900bb7159dcd68d15c3074c8
Guy Adam <guy.adam@gmail.com>
2017-04-26 18:06:58 +1000

    Testing mailable content

---
+++ tests/unit/Mail/OrderConfirmationEmailTest.php
@@ -0,0 +1,37 @@
+<?php 
+
+use App\Order;
+use App\Mail\OrderConfirmationEmail;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class OrderConfirmationEmailTest extends TestCase
+{
+    /** @test */
+    function email_contains_a_link_to_order_confirmation_page()
+    {
+        $order = factory(Order::class)->make([
+        	'confirmation_number' => 'ORDERCONFIRMATION1234',
+        ]);
+
+        $email = new OrderConfirmationEmail($order);
+        $rendered = $this->render($email);
+
+        $this->assertContains(url('/orders/ORDERCONFIRMATION1234'), $rendered);
+    }
+
+    /** @test */
+    function email_has_a_subject()
+    {
+        $order = factory(Order::class)->make();
+        $email = new OrderConfirmationEmail($order);
+		$this->assertEquals("Your TicketBeast Order", $email->build()->subject);
+    }
+
+    public function render($mailable)
+    {
+    	$mailable->build();
+    	return view($mailable->view, $mailable->buildViewData())->render();
+    }
+}
