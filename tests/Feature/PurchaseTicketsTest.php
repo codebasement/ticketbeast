cc791d940ecfa2190a2b17efc50bdc653b292658
Guy Adam <guy.adam@gmail.com>
2017-11-30 08:14:41 +1100

    Removed commented code from PurchaseTicketsTest.php after 'Custom factory classes'

--- tests/Feature/PurchaseTicketsTest.php
+++ tests/Feature/PurchaseTicketsTest.php
@@ -63,12 +63,6 @@ class PurchaseTicketsTest extends TestCase
         OrderConfirmationNumber::shouldReceive('generate')->andReturn('ORDERCONFIRMATION1234');
         TicketCode::shouldReceive('generateFor')->andReturn('TICKETCODE1', 'TICKETCODE2', 'TICKETCODE3');
 
-    	// $concert = factory(Concert::class)->create([
-     //        'ticket_price' => 3250,
-     //        'ticket_quantity' => 3,
-     //    ]);
-     //    $concert->publish();
-
         $concert = \ConcertFactory::createPublished([
             'ticket_price' => 3250,
             'ticket_quantity' => 3,
